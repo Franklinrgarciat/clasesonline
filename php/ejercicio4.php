81e226ab30124f73542414c3dee36ff32ed33b5a
ccharaco <jcesarchg9@gmail.com>
2020-07-25 18:48:36 -0400

    ejercicio 4 listo

--- php/ejercicio4.php
+++ php/ejercicio4.php
@@ -10,10 +10,13 @@ extract($_REQUEST);
 	<title>Mi Formulario</title>
 	<link rel="stylesheet" href="">
 	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 	<style type="text/css" media="screen">
+
 		*{
 			color: white;
-			background-color: green;
+			background-color: #666;
 			font-family: Arial;
 		}
 		label{
@@ -36,45 +39,95 @@ extract($_REQUEST);
 			background-color: #666;
 
 		}
+	.mensaje{
+		color:red;
+	}
 	</style>
 </head>
-<body><div class="container">
+<body>
+	<div class="container">
 	
-	<div class="navbar-header">
+		<div class="navbar-header">
 	
-	<h4>Mi Primer Formulario</h4>
+			<h4>Mi Primer Formulario</h4>
+		</div>
+	<div class="row">
+		<div class="col-12">
+			<?php 
+			if (isset($mensaje) ) {
+				?>
+			<div class="alert alert-primary" role="alert">
+				<?php if($mensaje==0){
+			 		echo "Monto restante a pagar:".$resta." en ".$cuotas." cuotas de ".$cuota." c/u";
+				}else{
+					echo "NO PUEDE HACERLO";
+				} ?>
+			</div>
+	<?php	} ?>		
+		</div>
 	</div>
-	<?php 
-	//echo $mensaje;
-	if (isset($mensaje) ) {
-
-		if($mensaje==1){
-		?>
-		<div class="alert alert-primary" role="alert">
-		  PUEDE COMPRAR
-		</div>
-	<?php
-		}else{
-		?>
-		<div class="alert alert-primary" role="alert">
-		  NO PUEDE HACERLO
-		</div>
-	<?php	
-		}
-	}?>
 	<form action="ejercicio4_submit.php" method="post" name="miformulario" enctype="Multipart/Form-data" accept-charset="utf-8" >
-		
-	<div class="form" align="center" >
-	<label>Nombre</label> <input type="text" name="nombre" value="" placeholder="Ingrese su Nombre"><br>
-	<label>Apellido</label> <input type="text" name="apellido" value="" placeholder="Ingrese su Apellido"><br>
-	<label>Edad</label> <input type="text" name="edad" value="" placeholder="Ingrese su Edad"><br>
-	<label>Presupuesto</label> <input type="text" name="Presupuesto" value="" placeholder="Ingrese su Presupuesto"><br>
-	<label>Nacionalidad</label> <input type="text" name="Nacionalidad" value="" placeholder="Igrese su Nacionalidad"><br>
-	<div class="botones">
-	<input class="btn-primary" type="reset" name="Limpiar" value="Limpiar">
-	<input class="btn-info" type="submit" name="Enviar" value="Enviar">
+	<div class="row">
+		<div class="col" align="center">
+			<small>(<span class="mensaje">*</span>) Los Campos son obligatorios</small>
+		</div>
+	</div>
+	<div class="row form-group">
+		<div class="col-6" align="right"><span class="mensaje">*</span> <label>Nombre</label></div>
+		<div class="col-3"><input class="form-control" required="required" type="text" name="nombre" value="" placeholder="Ej: Franklin" title="Ingrese su nombre">
+			<small><span class="mensaje">Ingrese su nombre</span></small>
+		</div>
 		
 	</div>
+	<div class="row form-group">
+		
+		<div class="col-6" align="right"><span class="mensaje">*</span> <label>Apellido</label></div>
+		<div class="col-3"><input title="Ingrese su apellido" required="required" class="form-control" type="text" name="apellido" value="" placeholder="Ej: Garcia">
+			<small><span class="mensaje">Ingrese su apellido</span></small>
+		</div>
+	</div>
+	<div class="row form-group">
+		
+		<div class="col-6" align="right"><span class="mensaje">*</span> <label>Cédula</label></div>
+		<div class="col-1">
+			<select name="nacionalidad" class="form-control" title="Seleccione la nacionalidad">
+				<option value="V" title="Venezolan@">V</option>
+				<option value="A" title="Argentin@">A</option>
+			</select>
+		</div>
+		<div class="col-2">
+			<input class="form-control" type="number" required="required" title="Ingrese se Cédula" name="cedula" value="" placeholder="Ej: 12121211">
+			<small><span class="mensaje">Ingrese su cédula</span></small></div>
+		
+	</div>
+	<div class="row form-group">
+		
+		<div class="col-6" align="right"><span class="mensaje">*</span> <label>Edad</label></div>
+		<div class="col-3"><input class="form-control" title="Ingrese la Edad" type="number" maxlength="2" minlength="2" name="edad" value="" placeholder="Ej: 18" size="4" required="required">
+			<small><span class="mensaje">Ingrese su edad</span></small></div>
+		
+	</div>
+	<div class="row form-group">
+		
+		<div class="col-6" align="right"><span class="mensaje">*</span> <label>Valor de la Casa</label></div>
+		<div class="col-3"><input class="form-control" required="required" type="number" title="Ingrese el valor de la casa" name="monto_casa" value="0" placeholder="Ej: 500000">
+			<small><span class="mensaje">Ingrese el valor de la casa</span></small>
+		</div>
+		
+	</div>
+	<div class="row form-group">
+		
+		<div class="col-6" align="right"><span class="mensaje">*</span> <label>Presupuesto para la Casa</label></div>
+		<div class="col-3"><input class="form-control" required="required" type="number" title="Ingrese su presupuesto para la compra de la casa" name="presupuesto" value="0" placeholder="Ej: 20000">
+			<small><span class="mensaje">Ingrese su presupuesto</span></small>
+		</div>
+	</div>
+	<div class="row form-group" >
+		
+		<div class="col-6"></div>
+		<div class="col-3"><input class="btn-primary" type="reset" name="Limpiar" value="Limpiar"><input class="btn-info" type="submit" name="Enviar" value="Enviar"></div>
+		<div class="col-3"></div>
+		
 	</div>
 	
 	</form>
@@ -83,4 +136,5 @@ extract($_REQUEST);
 <script src="jquery.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 </html>
--- php/ejercicio4_submit.php
+++ php/ejercicio4_submit.php
@@ -3,14 +3,54 @@
 extract($_REQUEST);
 //echo $_POST['nombre'];
 //echo $nombre." ".$apellido." ".$cedula;
-if(isset($nombre) && isset($apellido) && isset ($edad) && isset($nacionalidad) && isset($presupuesto)){
-	echo "no puede comprar";
-	}else{
-	echo "si Puede Comprar";
+
+if($nombre!="" && $apellido!="" && $edad!="" && $nacionalidad!="" && $cedula!="" && $monto_casa!="" && $presupuesto!=""){
+		
+	if ($nacionalidad=="V") {
+		if ($edad>=18) {
+			# verificacion cuando es mayor o igual a 50%
+			$porcentaje_50=(50*$monto_casa)/100;
+			$porcentaje_25=(25*$monto_casa)/100;
+			if ($presupuesto>=$porcentaje_50) {
+				$porcentaje_10=(10*$monto_casa)/100;
+				$monto_casa+=$porcentaje_10;
+				$resta=$monto_casa-$presupuesto;
+				$cuotas=12;
+				$cuota=$resta/$cuotas;
+				$mensaje=0;
+				header('Location: ejercicio4.php?mensaje='.$mensaje."&resta=".$resta."&cuotas=".$cuotas."&cuota=".$cuota);
+			} else if($presupuesto>=$porcentaje_25) {
+				$porcentaje_15=(15*$monto_casa)/100;
+				$monto_casa+=$porcentaje_15;
+				$resta=$monto_casa-$presupuesto;
+				$cuotas=24;
+				$cuota=$resta/$cuotas;
+				$mensaje=0;
+				header('Location: ejercicio4.php?mensaje='.$mensaje."&resta=".$resta."&cuotas=".$cuotas."&cuota=".$cuota);
+			}else{
+				
+				$mensaje=1;
+				header('Location: ejercicio4.php?mensaje='.$mensaje);
+			}
+			
+		} else {
+			
+			$mensaje=1;
+			header('Location: ejercicio4.php?mensaje='.$mensaje);
+		}
+	} else {
+		
+		$mensaje=1;
+		header('Location: ejercicio4.php?mensaje='.$mensaje);
+	}
+}else{
+	
+	$mensaje=1;
+	header('Location: ejercicio4.php?mensaje='.$mensaje);
+	
 }
 
 
-$mensaje=1;
 
 
 
@@ -19,6 +59,6 @@ $mensaje=1;
 
 
 
-//header('Location: ejercicio4.php?mensaje='.$mensaje);
+
 
 ?>
