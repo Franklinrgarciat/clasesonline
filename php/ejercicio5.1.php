fa5084e6765fc473bc8a72ffd6b1a60111787275
ccharaco <jcesarchg9@gmail.com>
2020-08-02 20:41:37 -0400

    cambios en formulario

--- php/ejercicio5.1.php
+++ php/ejercicio5.1.php
@@ -35,61 +35,64 @@
 	</div>
 	<div class="container-fluid">
 		<div class="row">
+			<div class="col-sm-4">
+				
+			</div>
+			<div class="col-sm-1 form-group">
+				<label for="nombre">Nombre</label>
+			</div>
+			<div class="col-sm-4 form-group">
+				<input type="text" name="nombre" placeholder="Ej: Cesar" class="form-control">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4">
+				
+			</div>
+			<div class="col-sm-1 form-group">
+				<label for="apellido">Apellido</label>
+			</div>
+			<div class="col-sm-4 form-group">
+				<input type="text" name="apellido" placeholder="Ej: Characo" class="form-control">
+			</div>
+		</div>
+		<div class="row">
 			<div class="col-md-2">
 				
 			</div>
-		
-		<div>
-			<?php 
-			if (isset($mensaje) ) {
-				?>
-			<div class="alert alert-primary" role="alert">
-				<?php if($mensaje==0){
-			 		echo "Si puede comprar; Monto restante a pagar:".$resta." en ".$cuotas." cuotas de ".$cuota." c/u";
-				}else{
-					echo "NO PUEDE COMPRAR, DICULPE!!";
-				} ?>
-			</div>
-	<?php	} ?>		
-		</div>
-	
-		
+			<form action="ejercicio5.2.php" method="post" name="miformulario" enctype="Multipart/Form-data" accept-charset="utf-8" >
 			<div class="col-md-4">
-			<div class="form-group" align="right">
-				<form action="ejercicio5.2.php" method="post" name="miformulario" enctype="Multipart/Form-data" accept-charset="utf-8" >
+				<div class="form-group" align="right">
 					<div class="datos">
-						<br><br><br><br>
-				<ul>
-							
-				<label><li>Nombre</li></label><br><br>
-				<label><li>Apellido</li></label><br><br>
-				<label><li>Cedula</li></label><br><br>
-				<label><li>Edad</li></label><br><br>
-				<label><li>Valor de la casa</li></label><br><br>
-				<label><li>Presupuesto</li></label><br><br>
-				<br>
+						<ul>
+							<label class="label"><li>Nombre</li></label><br><br>
+							<label><li>Apellido</li></label><br><br>
+							<label><li>Cedula</li></label><br><br>
+							<label><li>Edad</li></label><br><br>
+							<label><li>Valor de la casa</li></label><br><br>
+							<label><li>Presupuesto</li></label><br><br>
+							<br>
 						</ul>		
 					</div>
-		</div>
+				</div>
 			</div>
-			<div class="col-md-4">
+			<div class="col-md-4 form-group">
 				<br><br><br><br>
-					<input type="text" name="nombre" value="" style="background-color: white;" required="required" placeholder="Ingrese su nombre"><br><br>
-					<input type="text" name="apellido" value="" style="background-color: white;" required="required" placeholder="Ingrese su Apellido"><br><br>
-					<input type="number" name="cedula"  style="background-color: white;" required="required" placeholder="Ingrese su Cedula"><br><br>
-					<input type="number" name="edad" value="" style="background-color: white;" required="required" maxlength="2" minlength="2" placeholder="Ingrese su Edad"><br><br>
-					<input type="number" name="monto_casa" style="background-color: white;" value="" required="required" placeholder="Ingrese el Valor "><br><br>
-					<input type="number" name="presupuesto" style="background-color: white;" value="" required="required" placeholder="Ingrese su Presupuesto"><br><br>
-  <a href="http://localhost/clasesonline/php/ejercicio5.2.php" class="btn btn-primary" type="submit">Enviar Formulario</a>
+					<input type="text" class="form-control" name="nombre" value="" style="background-color: white;" required="required" placeholder="Ingrese su nombre"><br><br>
+					<input type="text" class="form-control" name="apellido" value="" style="background-color: white;" required="required" placeholder="Ingrese su Apellido"><br><br>
+					<input type="number" class="form-control" name="cedula"  style="background-color: white;" required="required" placeholder="Ingrese su Cedula"><br><br>
+					<input type="number" class="form-control" name="edad" value="" style="background-color: white;" required="required" maxlength="2" minlength="2" placeholder="Ingrese su Edad"><br><br>
+					<input type="number" class="form-control" name="monto_casa" style="background-color: white;" value="" required="required" class="form-control" placeholder="Ingrese el Valor "><br><br>
+					<input type="number" class="form-control" name="presupuesto" style="background-color: white;" value="" required="required" placeholder="Ingrese su Presupuesto"><br><br>
+  					<a href="http://localhost/clasesonline/php/ejercicio5.2.php" class="btn btn-primary" type="submit">Enviar Formulario</a>
 			</div>
 			
-	</form>
-	
-	</div>
+			</form>
+		</div>
 	<footer style="text-align: center;">
 		<br><i><b>Por Franklin Garcia - Inmo-Lider-SRL<b></i><br>
 	</footer>
-	
+	</div>
 </body>
 <script src="clasesonline/javascript/jquery.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
