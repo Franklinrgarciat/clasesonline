1a69dba876d5bce9ad35eb886f8287c6c74ab87d
Franklinrgarciat <franklingt_10@hotmail.com>
2020-09-05 21:15:45 -0300

    correcciones

--- php/ejercicios/new_calculo.php
+++ php/ejercicios/new_calculo.php
@@ -2,8 +2,8 @@
 extract($_REQUEST);
 
 if (isset($enviada)) {
-if (!empty($base) || !empty($altura)) {
-	$calculo1 = $base * $altura;
+if (!empty($base)) {
+	$calculo1 = pow($base, 2);
 	$resultado1 = true;
 }else{
 	$resultado1 = false;
@@ -23,20 +23,20 @@ if (!empty($radio)) {
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
-	<title>Calculando Areas</title>
+	<title>Calculando Áreas</title>
 </head>
 <body>
 	<?php 
 	if ($enviada == 1) {
 		if ($resultado1) {
-			echo "EL AREA DE UN CUADRADO ES :".$calculo1."<br>";
+			echo "EL ÁREA DE UN CUADRADO ES :".$calculo1."<br>";
 		}else{
-			echo "FALTAN VALORES";
+			echo "FALTAN VALORES<br>";
 		}
 		if ($resultado2) {
-			echo "EL AREA DE UN CIRCULO ES :".$calculo2."<br>";
+			echo "EL ÁREA DE UN CIRCULO ES :".$calculo2."<br>";
 		}else{
-			echo "FALTAN VALORES";
+			echo "FALTAN VALORES<br>";
 		}
 	}
 
@@ -45,15 +45,13 @@ if (!empty($radio)) {
 
 	<form action="" method="POST">
 		
-	<h2>CALCULANDO EL AREA DE UN CUADRADO</h2>
-	<label>INGRESE LA BASE DEL CUADRADO</label><br>
+	<h2>CALCULANDO EL ÁREA DE UN CUADRADO</h2>
+	<label>INGRESE LA MEDIDA DE UN LADO DEL CUADRADO</label><br>
 	<input type="number" name="base"><br><br>
-	<label>INGRESE LA ALTURA DEL CUADRADO</label><br>
-	<input type="number" name="altura"><br><br>
 	
 	<hr>
-	<h2>CALCULANDO EL AREA DE UN CIRCULO</h2>
-	<label>INGRESE EL RADIO DE UN CIRCULO</label><br>
+	<h2>CALCULANDO EL ÁREA DE UN CÍRCULO</h2>
+	<label>INGRESE EL RADIO DE UN CÍRCULO</label><br>
 	<input type="text" name="radio"><br><br>
 	<input type="hidden" value="1" name="enviada">
 	<input type="submit" name="enviar" value="enviar">
